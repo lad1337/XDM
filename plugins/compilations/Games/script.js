6496aaf2bea72683e4fa15d67d0e1af4e4d7fd8f
Dennis Lutter <lad1337@gmail.com>
2013-06-27 19:50:47 +0200

    make use of the de_lad1337_games_init() js call

--- plugins/compilations/Games/script.js
+++ plugins/compilations/Games/script.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function() {
+    de_lad1337_games_init();
+});
+
+
+function de_lad1337_games_init(){
 
     $('.de-lad1337-games .tabbable .tab-pane').each(function(index, item){
         $(item).appendTo(".de-lad1337-games .tab-content");
         $(item).removeClass('hidden');
     });
     
-});
+    $(".de-lad1337-games a.trailer").YouTubePopup({'clickOutsideClose':true, 'hideTitleBar':true});
+     
+}
