6b4be60812626bc0ab3f13cad9a1308bdddb8d0d
Dennis Lutter <lad1337@gmail.com>
2013-08-03 00:05:45 +0200

    same difference fix git update returning correct result

--- xdm/updater.py
+++ xdm/updater.py
@@ -353,7 +353,7 @@ class GitUpdateManager(UpdateManager):
     def update(self):
         common.SM.setNewMessage('Init git pull on')
         pull = git.pull(_cwd=xdm.APP_PATH)                   # get the local repo
-        if pull.exit_code == 0:
+        if pull.exit_code != 0:
             log.warning("Git pull exited with an exit code %s\n %s" % (pull.exit_code, pull))
             return False
         else:
